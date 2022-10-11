98f00a91e10c826e65ba4edd61f6fcc57d611633
AlekseiByk <bykov.as@phystech.edu>
2022-10-12 02:30:33 +0300

    parsing of log files and displaying usage of current

---
+++ Plugs/parse.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import datetime
+import time
+
+log_file = open("Plugs.log", "r")
+Lines = log_file.readlines()
+
+i = 0
+x = [[],[],[],[],[],[],[]]
+y = [[],[],[],[],[],[],[]]
+for line in Lines:
+    obj = datetime.datetime.strptime(line.split(' ')[0] + " " + line.split(' ')[1], "%Y-%m-%d %H:%M:%S,%f")
+    sec = obj.time().hour * 60 * 60 + obj.time().minute * 60 + obj.time().second
+    x[int(line.split(' ')[4]) - 1].append(sec/3600)
+    y[int(line.split(' ')[4]) - 1].append(int(line.split(' ')[9]))
+
+for x_cord in x:
+    plt.figure(figsize=[48, 16], dpi=120)
+    plt.plot(x_cord, y[i], label = f"washer {i + 1}")
+    for x_x in range(12):
+        plt.axvline(x_x * 2, color = "r")
+    plt.xlabel('время, сек')
+    plt.ylabel('ток, мА')
+    plt.xticks(range(25))
+    plt.savefig(f'output/plot{i+1}.png')
+    i += 1
+
+plt.show()
+
