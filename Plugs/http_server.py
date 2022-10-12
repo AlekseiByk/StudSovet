30477c4be665f7b2252e7905b406b6e6865621a5
Aleksei Bykov <55793148+AlekseiByk@users.noreply.github.com>
2022-10-12 14:00:37 +0300

    create http_server.py
    server for receiving messages from 'plugs controller' and notificate users

---
+++ Plugs/http_server.py
@@ -0,0 +1,55 @@
+import http.server
+import logging
+
+addr = "0.0.0.0"
+port = 8000
+secret = ''
+
+logger = logging.getLogger("Plugs")
+
+# ensure dual-stack is not disabled; ref #38907
+class DualStackServer(http.server.ThreadingHTTPServer):
+    def server_bind(self):
+        # suppress exception when protocol is IPv4
+        with http.server.contextlib.suppress(Exception):
+            self.socket.setsockopt(
+                http.server.socket.IPPROTO_IPV6, http.server.socket.IPV6_V6ONLY, 0)
+        return super().server_bind()
+
+class MyRequestHandler(http.server.SimpleHTTPRequestHandler):                   #Heandler for GET Reguest 
+    def do_GET(self):
+        self.send_response(http.server.HTTPStatus.NOT_FOUND)
+    def do_POST(self):
+        if not self.path == ('/' + secret):
+            self.send_response(http.server.HTTPStatus.NOT_FOUND)
+            return
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = post_data.decode('ascii')
+        if data.split('=')[0] == 'finished':
+            washer = int( data.split('=')[1])
+            #TODO send notification to user
+        elif data.split('=')[0] == 'log':
+            logger.info(f"washer {data.split('=')[1] : 02} - " + data.split('=')[2])
+        self.send_response(http.server.HTTPStatus.OK)
+        self.end_headers()
+
+def set_logger(logger: logging.Logger, filename):
+    format = "%(asctime)s >>> %(message)s"
+    logging.basicConfig(level=logging.INFO, format=format)
+#    filehand = logging.FileHandler(f"/home/admin/ssFRKT-bot/logs/{filename}")
+    filehand = logging.FileHandler(f"{filename}") 
+    filehand.setFormatter(logging.Formatter(format))
+    logger.addHandler(filehand)
+
+def run():
+    set_logger(logger, "http_server.log")
+    server_addr = (addr, port)
+    httpd = DualStackServer(server_addr,MyRequestHandler)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+
+if __name__ == "__main__":
+    run()
